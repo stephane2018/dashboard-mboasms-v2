e97b7147c51d14c6d6ce6fd3d38a69b97d6ab5fa
stephane2018 <31846762+stephane2018@users.noreply.github.com>
2026-04-23 14:32:55 +0100

    Exclude sitemap.xml and robots.txt from auth middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -75,6 +75,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.svg$).*)',
   ],
 }
